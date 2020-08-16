a39eea8256e48b9080b9dea6a5538f6ef0753c30
siddhart <siddhart@montaigne.co>
2020-08-16 19:15:07 +0530

    Added Complete GUI for App2

--- apps/app2.py
+++ apps/app2.py
@@ -61,8 +61,8 @@ layout = html.Div([
                html.H5("Year Selection-1:", className="text-center"),
             dcc.Dropdown(
                 id="year_sel_1",
-                options=[],
-                className="dcc_control"
+                # options=[],
+                # className="dcc_control"
             )
            ])
         ),
@@ -71,8 +71,8 @@ layout = html.Div([
             html.H5("Year Selection-2:", className="text-center"),
             dcc.Dropdown(
                 id="year_sel_2",
-                options=[],
-                className="dcc_control"
+                # options=[],
+                # className="dcc_control"
             )
         ])
     )], style={"padding":45}),
@@ -87,61 +87,25 @@ layout = html.Div([
 
         )
     # ])
-    ])])
+])])
+# class corr_gen:
+@app.callback(
+    Output("year_sel_1","options"),
+    [Input("CHOICE_1","value")])
+def drpdwn_1_opt(ch1):
+    df1 = pd.read_csv(ch1)
+    y1 = list(df1['Year'].unique())
+    y2 = list(df1['Year'].unique())
+    drdp1 = dict(zip(y1,y2))
+    return [{"label":str(i), "value":drdp1[i]}for i in drdp1]
 
 
 @app.callback(
-Output('example-output1','children'),
-#
-    [Input('CHOICE_1','value'), Input('CHOICE_2', 'value'), Input('Country', 'value'), Input('example-button','n_clicks')]
-)
-def corr_gen(ch1, ch2, count, n):
-
-    print(type(ch2))
-
-    #
-    # str(ch1)
-    # str(ch2)
-    # str(count)
-
-    df1 = pd.read_csv(ch1)
-    df2 = pd.read_csv(ch2)
-
-    df_1 = df1.loc[(df1["Entity"] == count)]
-    df_2 = df2.loc[(df2["Entity"] == count)]
-
-    df_all = df_2.merge(df_1.drop_duplicates(), on="Year", how="left", indicator=False)
-
-    df_all.rename(columns={df_all.columns[3]: "Data-1"}, inplace=True)
-    df_all.rename(columns={df_all.columns[6]: "Data-2"}, inplace=True)
-
-    df_all = df_all[df_all['Data-1'].notna()]
-    df_all = df_all[df_all['Data-2'].notna()]
-
-    X = df_all["Data-1"]
-    Y = df_all["Data-2"]
-
-    pearson_coef1 = np.corrcoef(X, Y)
-    pearson_coef = pearson_coef1[1, 0]
-    int(pearson_coef)
-
-    # pearson_coef = 12929292
-    # str(pearson_coef)
-    # print(pearson_coef)
-    if n is None:
-        return "Not clicked."
-    else:
-        return f"Clicked {pearson_coef} times."
-
-
-
-    # return html.Div('The value is:"{}" '.format(pearson_coef))
-# @app.callback(
-# Output("Country", "options"),
-#     [Input("CHOICE_1", "value"), Input("CHOICE_2","value")]
-# )
-# def load_country(ch1, ch2):
-#     if ch1 == "" and ch2 == "":
-#         print("Input Invalid")
-#     print(ch1)
-#     print(ch2)
+    Output("year_sel_2","options"),
+    [Input("CHOICE_2","value")])
+def drpdwn_2_opt(ch2):
+    df_2 = pd.read_csv(ch2)
+    y_1 = list(df_2['Year'].unique())
+    y_2 = list(df_2['Year'].unique())
+    drdp2 = dict(zip(y_1,y_2))
+    return [{"label":str(i), "value":drdp2[i]}for i in drdp2]
